c0f2163a8462930026021940b09df6c31aa44cf4
moi3n <moi3nr@gmail.com>
2023-04-08 13:59:29 +0430

    added function to plot ouput of s1s2datset

---
+++ dataset/utils/plot_utils.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import torch
+
+def plot_s1s2_tensors(tensors, names, n_rows, n_cols):
+    fig, axs = plt.subplots(n_rows, n_cols)
+    for i in range(n_rows):
+        for j in range(n_cols):
+            idx = i * n_cols + j
+            if idx >= len(tensors):
+                break
+            tensor = tensors[idx].to(torch.float32)
+            if torch.min(tensor) < 0:
+                tensor = (tensor + 1)/2
+            name = names[idx] if names is not None else None
+            if tensor.ndim > 2 and tensor.shape[0] > 1:
+                axs[i][j].imshow(tensor[[3,2,1],:,:].permute(1,2,0).cpu().numpy())
+                axs[i][j].set_title(name)
+            else:
+                axs[i][j].imshow(tensor[0].cpu().numpy())
+                axs[i][j].set_title(name)
+            axs[i][j].set_xticks([])
+            axs[i][j].set_yticks([])
+    plt.show()
