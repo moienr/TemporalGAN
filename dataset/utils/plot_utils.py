b57ee7c56bd7af2a0ec43883428ce7d85ab74457
moi3n <moi3nr@gmail.com>
2023-04-10 00:22:38 +0330

    added a funciton to plot tesnors and save the plot as  a png file

--- dataset/utils/plot_utils.py
+++ dataset/utils/plot_utils.py
@@ -21,3 +21,25 @@ def plot_s1s2_tensors(tensors, names, n_rows, n_cols):
             axs[i][j].set_xticks([])
             axs[i][j].set_yticks([])
     plt.show()
+    
+    
+def save_s1s2_tensors_plot(tensors, names, n_rows, n_cols, filename, fig_size):
+    fig, axs = plt.subplots(n_rows, n_cols, figsize=fig_size)
+    for i in range(n_rows):
+        for j in range(n_cols):
+            idx = i * n_cols + j
+            if idx >= len(tensors):
+                break
+            tensor = tensors[idx].to(torch.float32)
+            if torch.min(tensor) < 0:
+                tensor = (tensor + 1)/2
+            name = names[idx] if names is not None else None
+            if tensor.ndim > 2 and tensor.shape[0] > 1:
+                axs[i][j].imshow(tensor[[3,2,1],:,:].permute(1,2,0).cpu().numpy())
+                axs[i][j].set_title(name)
+            else:
+                axs[i][j].imshow(tensor[0].cpu().numpy())
+                axs[i][j].set_title(name)
+            axs[i][j].set_xticks([])
+            axs[i][j].set_yticks([])
+    plt.savefig(filename)
