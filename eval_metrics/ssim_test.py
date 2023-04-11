1d03b97aa560990fc25e648b7c9dbba2762cf8bb
moi3n <moi3nr@gmail.com>
2023-04-11 21:54:57 +0330

    copied them from pytorch ignite library to change them in weighted format

---
+++ eval_metrics/ssim_test.py
@@ -0,0 +1,12 @@
+import torch
+from torchvision.transforms import functional as F
+from ignite.metrics import SSIM
+
+# Create two dummy tensors
+tensor1 = torch.rand(1, 1, 256, 256)
+tensor2 = tensor1.clone() + torch.rand(1, 1, 256, 256) * 0.1
+# Apply SSIM on the tensors
+ssim = SSIM(data_range=1.0)
+ssim.update((tensor1, tensor2))
+print(ssim.compute())
+print(tensor1.size(2))
