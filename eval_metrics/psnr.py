194857593cb8c162fd3acbd2b2d382c1f0366f46
moi3n <moi3nr@gmail.com>
2023-04-13 12:51:42 +0330

    minor | fixed velue error message

--- eval_metrics/psnr.py
+++ eval_metrics/psnr.py
@@ -28,7 +28,7 @@ def wpsnr(compare_tesnors: Sequence[torch.Tensor], weight_map: torch.Tensor = No
         raise ValueError(f"Images and weight map must have the same shape. Got {y.shape}, {y_pred.shape}, and {weight_map.shape}")
     
     if len(weight_map.shape) != 4 or len(y.shape) != 4:
-        raise ValueError(f"Expected weight_mapm y and y_pred to be 4D tensors. Got weight_map: {weight_map.shape} and y: {y.shape}, and y_pred: {y_pred.shape}.")
+        raise ValueError(f"Expected weight_map, y and y_pred to be 4D tensors. Got weight_map: {weight_map.shape} and y: {y.shape}, and y_pred: {y_pred.shape}.")
 
     if weight_map.shape[0] != y.shape[0] or (weight_map.shape[1] != y.shape[1] and weight_map.shape[1] != 1) or weight_map.shape[2] != y.shape[2] or weight_map.shape[3] != y.shape[3]:
         raise ValueError(
