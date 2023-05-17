bf4d9c3328766d7cbdcfd522b4c94ecfe02279c1
moi3n <moi3nr@gmail.com>
2023-05-17 20:14:24 +0330

    added eval funcitons from notebook

--- train_utils.py
+++ train_utils.py
@@ -9,7 +9,7 @@ from eval_metrics.loss_function import reverse_map
 from changedetection.utils import get_binary_change_map
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
-def save_some_examples(gen, val_dataset ,epoch, folder, cm_input, img_indx = 1, just_show = False):
+def save_some_examples(gen, val_dataset ,epoch, folder, cm_input, img_indx = 1, just_show = False, fig_size = (8,12)):
     s2t2,s1t2,s2t1,s1t1,cm,rcm,s1cm  = val_dataset[img_indx]
     s2t2,s1t2,s2t1,s1t1,cm,rcm,s1cm = s2t2.to(DEVICE),s1t2.to(DEVICE),s2t1.to(DEVICE),s1t1.to(DEVICE),cm.to(DEVICE),rcm.to(DEVICE),s1cm.to(DEVICE)
     if cm_input:
@@ -23,8 +23,9 @@ def save_some_examples(gen, val_dataset ,epoch, folder, cm_input, img_indx = 1, 
     with torch.no_grad():
         s1t2_generated = gen(s2t2.unsqueeze(0).to(torch.float32), s1t1.unsqueeze(0).to(torch.float32))
         
-        s1cm_binary = get_binary_change_map(s1cm.to(torch.float32))
-        s1cm_binary_reverse = reverse_map(s1cm_binary)
+        s1cm_binary = get_binary_change_map(s1cm.to(torch.float32))  * 1.0
+
+        s1cm_binary_reverse = torch.tensor(1.0) - s1cm_binary
         
         weighted_ssim = wssim((s1t2.unsqueeze(0).to(torch.float32), s1t2_generated.to(torch.float32)), s1cm_binary.unsqueeze(0))
         normal_ssim = wssim((s1t2.unsqueeze(0).to(torch.float32), s1t2_generated.to(torch.float32)))
@@ -47,20 +48,26 @@ def save_some_examples(gen, val_dataset ,epoch, folder, cm_input, img_indx = 1, 
         s1t2_generated_change_highlited = s1t2_generated_diff_w_s1t2 * (s1cm_binary + 0.1)
             
         if cm_input:
-            input_list = [s2t1,s1t1[0].unsqueeze(0),s2t2,s1t2,torch.abs(cm),s1cm,rcm,s1t2_generated[0], s1cm_binary, s1t2_generated_diff_w_s1t2, s1t2_generated_diff_w_s1t1,s1t2_generated_change_highlited]
+            input_list = [s2t1, s2t2, torch.abs(cm), s1t1[0].unsqueeze(0),
+                          s1t2,s1cm,s1t2_generated[0],s1t2_generated[0], s1cm_binary,
+                          s1t2_generated_diff_w_s1t1, s1t2_generated_diff_w_s1t2,
+                          s1t2_generated_change_highlited]
         else:
-            input_list = [s2t1,s1t1,s2t2,s1t2,torch.abs(cm),s1cm,rcm,s1t2_generated[0]                , s1cm_binary, s1t2_generated_diff_w_s1t2, s1t2_generated_diff_w_s1t1,s1t2_generated_change_highlited]
+            input_list = [s2t1, s2t2, torch.abs(cm), s1t1,
+                          s1t2,s1cm,s1t2_generated[0],s1t2_generated[0], s1cm_binary,
+                          s1t2_generated_diff_w_s1t1, s1t2_generated_diff_w_s1t2,
+                          s1t2_generated_change_highlited]
             
         save_s1s2_tensors_plot(input_list,
-                               ["s2t1", "s1t1", "s2t2", "s1t2",
-                                "s2_change map", "s1_change map",
-                                "reversed change map" ,"generated s1t2",
-                                "s1_change map binary", "s1t2 generated diff w s1t2",
-                                "s1t2 generated diff w s1t1", "s1t2 generated change highlited"],
+                               ["s2t1","s2t2","s2_change map",
+                                "s1t1",  "s1t2","s1_change map",
+                                "generated s1t2" ,"generated s1t2",
+                                "s1_change map binary", "s1t2gen change from s1t1",
+                                "s1t2gen change from s1t2", "s1t2gen cm highlited"],
                                n_rows=4,
                                n_cols=3,
                                filename=f"{folder}//epoc_{epoch}_img{img_indx}.png",
-                               fig_size=(8,10),
+                               fig_size=fig_size,
                                title=title,
                                just_show= just_show)
     gen.train()
@@ -152,6 +159,137 @@ def train_fn(disc, gen, loader, opt_disc, opt_gen, l1_loss, bce, g_scaler, d_sca
                 L1_mean = sum(L1_list) / len(L1_list),
                 G_loss_mean = sum(G_loss_list) / len(G_loss_list),
             )
+
+
+def get_tensor_ones_ratio(tensor: torch.Tensor):
+
+    if len(tensor.shape) == 3:
+        tensor = tensor.unsqueeze(0)
+    ones_count = torch.sum(tensor)
+    total_pixels = tensor.shape[-1] * tensor.shape[-2] * tensor.shape[-3]
+    ratio = ones_count / total_pixels
+    
+    return ratio
+
+
+
+def batch_eval_loop(loss_fn, labels: torch.Tensor, preds: torch.Tensor, weight_maps: torch.Tensor = None, hard_test = False):
+    loss_list = []
+    for l, p, w in zip(labels, preds, weight_maps):
+        l, p, w = l.unsqueeze(0), p.unsqueeze(0), w.unsqueeze(0)
+        loss = loss_fn((l, p), w)
+        if hard_test:
+            changed_ratio = get_tensor_ones_ratio(w)
+        else:
+            changed_ratio = torch.tensor(1.0)
+        loss_list.append((loss, changed_ratio.item()))
+
+    return loss_list
+
+
+def weighted_mean(lst):
+    numerator = 0
+    denominator = 0
+    for tup in lst:
+        numerator += tup[0] * tup[1]
+        denominator += tup[1]
+    return numerator / denominator
+
+
+def eval_fn(gen, loader, ssim, psnr, hard_test = False, loader_part = "all"):
+    """_summary_
+
+    Args:
+        gen (torch.nn.Module): Generator model
+        loader (troch.utils.data.DataLoader): Data loader
+        ssim (torch.nn.Module): ssim loss function
+        psnr (torch.nn.Module): psnr loss function
+        hard_test (bool, optional): Wether to use hard test or not. Defaults to False.
+        loader_part (str, optional): Wether to use `all` of the loader or `first_half`  or `second_half` . Defaults to "all".
+
+    Returns:
+        dict: Dictionary of the evaluation metrics
+    """
+    gen.eval()
+    loop = tqdm(loader, leave=True)
+    weighted_ssim_list = []
+    normal_ssim_list = []
+    weighted_psnr_list = []
+    normal_psnr_list = []
+    reverse_weighted_ssim_list = []
+    reverse_weighted_psnr_list = []
+    
+    for idx, (s2t2,s1t2,s2t1,s1t1,cm,rcm,s1cm) in enumerate(loop):
+        if loader_part == "second" and idx < len(loader) // 2: # skip first half if second half is requested
+            continue 
+        s2t2,s1t2,s2t1,s1t1,cm,rcm,s1cm = s2t2.to(DEVICE),s1t2.to(DEVICE),s2t1.to(DEVICE),s1t1.to(DEVICE),cm.to(DEVICE),rcm.to(DEVICE),s1cm.to(DEVICE)
+        s2t2,s1t2,s2t1,s1t1,cm,rcm,s1cm = s2t2.to(torch.float32),s1t2.to(torch.float32),s2t1.to(torch.float32),s1t1.to(torch.float32),cm.to(torch.float32),rcm.to(torch.float32),s1cm.to(torch.float32)
+        if INPUT_CHANGE_MAP:
+            s2t2 = torch.cat((s2t2, cm), dim=1)
+            s1t1 = torch.cat((s1t1, rcm), dim=1)
+        preds = gen(s2t2, s1t1)
+        
+        s1cm_reversed = reverse_map(s1cm)
+        
+        # Rescaling from -1-1 to 0-1
+        s1t2 = (s1t2 + 1) / 2
+        preds = (preds + 1) / 2
+        
+        weighted_ssim = batch_eval_loop(ssim, s1t2, preds, s1cm, hard_test = hard_test)
+        weighted_ssim_list = weighted_ssim_list + weighted_ssim # weighted_ssim is a list of tuples
+        reverse_weighted_ssim = batch_eval_loop(ssim, s1t2, preds, s1cm_reversed, hard_test = hard_test)
+        reverse_weighted_ssim_list = reverse_weighted_ssim_list + reverse_weighted_ssim # weighted_ssim is a list of tuples
+        normal_ssim = ssim((s1t2, preds))
+        normal_ssim_list.append(normal_ssim)
+        
+        weighted_psnr = batch_eval_loop(psnr, s1t2, preds, s1cm, hard_test = hard_test)
+        weighted_psnr_list = weighted_psnr_list + weighted_psnr 
+        reverse_weighted_psnr = batch_eval_loop(psnr, s1t2, preds, s1cm_reversed, hard_test = hard_test)
+        reverse_weighted_psnr_list = reverse_weighted_psnr_list + reverse_weighted_psnr
+        normal_psnr = psnr((s1t2, preds))
+        normal_psnr_list.append(normal_psnr)
+
+
+        if idx % 10 == 0:
+            loop.set_postfix(
+                wssim_mean = weighted_mean(weighted_ssim_list),
+                ssim_mean = torch.tensor(normal_ssim_list).mean().item(),
+                rwssim_mean = weighted_mean(reverse_weighted_ssim_list),
+                wpsnr_mean = weighted_mean(weighted_psnr_list),
+                psnr_mean = torch.tensor(normal_psnr_list).mean().item(),
+                rwpsnr_mean = weighted_mean(reverse_weighted_psnr_list)
+                
+            )
+        if loader_part == "first" and idx >= len(loader) // 2: # stop after first half if first half is requested
+            break
+
+    
+    eval_dict = {"SSIM":{}, "PSNR":{}}
+    
+    eval_dict["SSIM"]["cwssim_mean"] = weighted_mean(weighted_ssim_list)
+    eval_dict["SSIM"]["ssim_mean"] = torch.tensor(normal_ssim_list).mean().item()
+    eval_dict["SSIM"]["rcwssim_mean"] = weighted_mean(reverse_weighted_ssim_list)
+    eval_dict["PSNR"]["cwpsnr_mean"] = weighted_mean(weighted_psnr_list)
+    eval_dict["PSNR"]["psnr_mean"] = torch.tensor(normal_psnr_list).mean().item()
+    eval_dict["PSNR"]["rcwpsnr_mean"] = weighted_mean(reverse_weighted_psnr_list)
+    
+    return eval_dict
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
